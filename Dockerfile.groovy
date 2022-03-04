e67c353e2cacbcee7bb68f35a829ef3f5aa33770
Aravindkumar P <aravind_p@blr.trigent.com>
2022-03-04 11:05:40 +0530

    third commit

--- Dockerfile.groovy
+++ Dockerfile.groovy
@@ -1,6 +1,6 @@
 FROM ubuntu:20.04 
 
 RUN apt-get update && apt-get install apache2 -y
-ENV DEBIAN_FRONTEND noninteractive
+ARG DEBIAN_FRONTEND=noninteractive
 
 CMD ["echo" , "web image created"]
--- Dockerfile.groovy.bak
+++ Dockerfile.groovy.bak
@@ -1,5 +1,6 @@
 FROM ubuntu:20.04 
 
 RUN apt-get update && apt-get install apache2 -y
+ENV DEBIAN_FRONTEND noninteractive
 
 CMD ["echo" , "web image created"]
